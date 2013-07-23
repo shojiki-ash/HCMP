9466adca01a4cd20eb35ec61074765c45670aa2c
shojiki-ash <ashminneh.mugo@gmail.com>
2013-07-23 14:05:42 +0300

    Update districts.php

--- application/models/districts.php
+++ application/models/districts.php
@@ -58,4 +58,26 @@ class Districts extends Doctrine_Record {
 			AND c.id='$county'");	
 		return $query;
 	}
+	public static function get_county_received($county){
+		$query=Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("SELECT o.id,o.orderDate,o.facilityCode, COUNT(o.facilityCode) as facility_count,o.deliverDate,o.remarks,o.orderStatus,o.dispatchDate,o.approvalDate,o.kemsaOrderid,o.orderTotal,o.status,o.orderby,o.order_no, f.facility_code, f.facility_name, d.id as district_id, d.district
+			FROM ordertbl o, facilities f, districts d, counties c
+			WHERE o.orderStatus='delivered'
+			AND o.facilityCode=f.facility_code
+			AND f.district=d.id
+			AND d.county=c.id
+			AND c.id='$county' GROUP BY d.district");
+		return $query;
+	}
+	public static function get_potential_expiry_summary($county){
+		$query=Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("SELECT COUNT(stock.facility_code) as facility_count, stock.facility_code, stock.balance,stock.quantity,stock.status,stock.stock_date,stock.sheet_no, f.facility_name, d.id as district_id, d.district
+			FROM Facility_Stock stock, facilities f, districts d, counties c
+			WHERE stock.expiry_date BETWEEN CURDATE()AND DATE_ADD(CURDATE(), INTERVAL 12 MONTH)
+			AND stock.status=1
+			AND stock.facility_code=f.facility_code
+			AND f.district=d.id
+			AND d.county=c.id
+			AND c.id='$county'
+			GROUP BY d.district");	
+		return $query;
+	}
 }
