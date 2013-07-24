8aabcff22ddf1f05d4222346a4eaaa6bcb4934ec
shojiki-ash <ashminneh.mugo@gmail.com>
2013-07-24 19:31:52 +0300

    Update user.php
    Added county column to setTableDefinition()

--- application/models/user.php
+++ application/models/user.php
@@ -9,6 +9,7 @@ class User extends Doctrine_Record {
 		$this->hasColumn('password', 'string', 255);
 		$this->hasColumn('usertype_id', 'integer', 11);
 		$this->hasColumn('telephone', 'varchar', 255);
+		$this->hasColumn('county', 'varchar', 255);
 		$this->hasColumn('district', 'varchar', 255);
 		$this->hasColumn('facility', 'varchar', 255);
 		$this->hasColumn('status', 'int', 11);
